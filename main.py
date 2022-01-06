54f23a5bf8d25e80b9baf36118eac2e7b543029d
Sakti Suman Tripathy <65999640+SaktiSuman@users.noreply.github.com>
2022-01-06 21:24:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import sys
+t1 = (1, 2, 3, 4)
+t2 = ("Geek1", "Raju", "Geek2", "Nikhil", "Geek3", "Deepanshu")
+t3 = ((1, "Lion"), ( 2, "Tiger"), (3, "Fox"), (4, "Wolf"))
+print("Size of Tuple1: " + str(sys.getsizeof(t1)) + "bytes")
+print("Size of Tuple2: " + str(sys.getsizeof(t2)) + "bytes")
+print("Size of Tuple3: " + str(sys.getsizeof(t3)) + "bytes")
